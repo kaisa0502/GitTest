5411f3ea12810fca19ff3f05b2649752e11c933f
kaisa.huang <kaisa0502@163.com>
2018-08-18 09:08:40 +0800

    update 002

--- git-test/AppDelegate.m
+++ git-test/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"---- kaisa");
+    NSLog(@"-----002");
     return YES;
 }
 
