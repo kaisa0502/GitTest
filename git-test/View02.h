cb42645401094e174090bde7721bda8703b19d46
kaisa.huang <kaisa0502@163.com>
2018-08-19 12:45:55 +0800

    02: add view02

---
+++ git-test/View02.h
@@ -0,0 +1,13 @@
+//
+//  View02.h
+//  git-test
+//
+//  Created by kaisa.huang on 2018/8/19.
+//  Copyright © 2018年 kaisa.huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View02 : UIView
+
+@end
