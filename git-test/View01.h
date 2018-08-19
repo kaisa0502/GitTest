6ed6bdc9630028d652389cf66025f8a99f7614f8
kaisa.huang <kaisa0502@163.com>
2018-08-19 12:45:32 +0800

    01: add view01

---
+++ git-test/View01.h
@@ -0,0 +1,13 @@
+//
+//  View01.h
+//  git-test
+//
+//  Created by kaisa.huang on 2018/8/19.
+//  Copyright © 2018年 kaisa.huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View01 : UIView
+
+@end
---
+++ git-test/View01.m
@@ -0,0 +1,21 @@
+//
+//  View01.m
+//  git-test
+//
+//  Created by kaisa.huang on 2018/8/19.
+//  Copyright © 2018年 kaisa.huang. All rights reserved.
+//
+
+#import "View01.h"
+
+@implementation View01
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
